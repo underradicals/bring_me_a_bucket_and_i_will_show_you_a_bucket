0bfd93668ae82623c516cd455d778a7b50748df3
Joseph <underradicals@gmail.com>
2025-04-10 15:10:07 -0500

    Add files

---
+++ src/D2App.Application/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace D2App.Application;
+
+public class Class1
+{
+
+}
---
+++ tests/D2App.Tests.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace D2App.Tests.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
